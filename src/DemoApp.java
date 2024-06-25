faa5c11bfc3d9f268fab0420eafd06d8f99579ce
ajlahoti <hoti.ajla@hotmail.com>
2024-06-25 13:08:08 +0200

    -probe

--- src/DemoApp.java
+++ src/DemoApp.java
@@ -13,7 +13,7 @@ public class DemoApp {
         Scanner input = new Scanner(System.in);
         PrintStream output = System.out;
         Spiel spiel = new Spiel(input, output);
-
+        System.out.println("Spiel....");
         // Startet das Spiel
         spiel.run();
         ArrayList<Spieler> spielerListe = spiel.getSpielerListe();
